5f35b586258a938fc7570f426cb84126d2bbb15f
evannn6 <gtevan48@gmail.com>
2025-01-01 19:19:40 +0700

    satu barang

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -14,7 +14,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div>
-                    <h3 class="text-center my-4">DAMN</h3>
+                    <h3 class="text-center my-4">APALAH</h3>
                     <hr>
                 </div>
                 <div class="card border-0 shadow-sm rounded">
